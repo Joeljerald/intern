a6757fd3ebb63b114a912f5dd696b34926c94eb4
Joeljerald <157976407+Joeljerald@users.noreply.github.com>
2025-02-01 13:10:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-// In your page file (e.g., src/pages/Home.jsx or src/App.jsx)
+
 import BifirstPromoGrid from './components/BifirstPromoGrid';
 
 function App() {
   return (
     <div>
       <BifirstPromoGrid />
-      {/* Other components */}
     </div>
   );
 }
